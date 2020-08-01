247c1ff57276c2e2a082533cbcaa504b22dfa7ca
lixiaohu123147 <7595644+lixiaohu123147@user.noreply.gitee.com>
2020-08-01 15:59:24 +0800

    将 svg 代码整理到 icon.vue 闭合标签

--- src/button.vue
+++ src/button.vue
@@ -1,10 +1,8 @@
 <template>
   <button class="g-button" :class="{[`icon-${iconPosition}`]: true}">
-    <svg v-if="icon" class="icon">
-      <use :xlink:href="`#i-${icon}`" />
-    </svg>
+    <g-icon class="icon" v-if="icon" :name="icon"></g-icon>
     <div class="content">
-      <slot></slot>
+      <slot />
     </div>
   </button>
 </template>
