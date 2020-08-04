1a7ea54719f80b136a951878d053e7eadaca9aaa
lixiaohu123147 <7595644+lixiaohu123147@user.noreply.gitee.com>
2020-08-04 10:01:32 +0800

    基本实现 row 和 col 的功能

--- src/row.vue
+++ src/row.vue
@@ -1,8 +1,33 @@
 <template>
-  <div class="row">
+  <div class="row" :style="{marginLeft: -gutter/2+'px', marginRight: -gutter/2+'px'}">
     <slot></slot>
   </div>
 </template> 
+
+<script>
+  export default {
+    name: 'GuluRow',
+    props: {
+      gutter: {
+        type: [Number, String]
+      }
+    },
+    created () {
+      console.log('row created')
+    },
+    mounted () {
+      console.log('row mounted')
+      console.log(this.$children)
+      this.$children.forEach((vm) => {
+        vm.gutter = this.gutter
+      })
+    }
+  }
+  var div = document.createElement('div') 
+  var childDiv = document.createElement('div') 
+  div.appendChild(childDiv) 
+  document.body.appendChild(div) 
+</script>
 
 <style lang="scss" scoped>
 .row {
