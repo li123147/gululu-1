61fbb9709cb3ea34ba38a9bd17e6c27e5c37f683
lixiaohu123147 <7595644+lixiaohu123147@user.noreply.gitee.com>
2020-08-05 16:40:59 +0800

    完成 toast 测试用例

--- src/toast.vue
+++ src/toast.vue
@@ -6,9 +6,7 @@
         <div v-else v-html="$slots.default[0]"></div>
       </div>
       <div class="line" ref="line"></div>
-      <span class="close" v-if="closeButton" @click="onClickClose">
-        {{closeButton.text}}
-      </span>
+      <span class="close" v-if="closeButton" @click="onClickClose">{{closeButton.text}}</span>
     </div>
   </div>
 </template>
@@ -18,12 +16,11 @@ export default {
   name: "GuluToast",
   props: {
     autoClose: {
-      type: Boolean,
-      default: true,
-    },
-    autoCloseDelay: {
-      type: Number,
-      default: 50,
+      type: [Boolean, Number],
+      default: 5,
+      validator(value) {
+        return value === false || typeof value === "number";
+      },
     },
     closeButton: {
       type: Object,
@@ -70,16 +67,13 @@ export default {
       if (this.autoClose) {
         setTimeout(() => {
           this.close();
-        }, this.autoCloseDelay * 1000);
+        }, this.autoClose * 1000);
       }
     },
     close() {
       this.$el.remove();
       this.$emit("close");
       this.$destroy();
-    },
-    log() {
-      console.log("测试");
     },
     onClickClose() {
       this.close();
@@ -126,27 +120,27 @@ $toast-bg: rgba(0, 0, 0, 0.75);
   position: fixed;
   left: 50%;
   transform: translateX(-50%);
-  $animation-duration:300ms;
-  &.position-top{
-    top:0;
-    .toast{
+  $animation-duration: 300ms;
+  &.position-top {
+    top: 0;
+    .toast {
       border-top-left-radius: 0;
       border-top-right-radius: 0;
       animation: slide-down $animation-duration;
     }
   }
-  &.position-bottom{
-    bottom:0;
-    .toast{
+  &.position-bottom {
+    bottom: 0;
+    .toast {
       border-top-left-radius: 0;
       border-top-right-radius: 0;
       animation: slide-up $animation-duration;
     }
   }
-  &.position-middle{
-    top:50%;
+  &.position-middle {
+    top: 50%;
     transform: translateX(-50%) translateY(-50%);
-    .toast{
+    .toast {
       animation: fade-in $animation-duration;
     }
   }
