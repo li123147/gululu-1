a629948df2028e8a6e1758e1a22fcc45b61ed389
lixiaohu123147 <7595644+lixiaohu123147@user.noreply.gitee.com>
2020-08-06 16:21:20 +0800

    添加部分测试用例

--- src/tabs-item.vue
+++ src/tabs-item.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="tabs-item" @click="onClick" :class="classes">
+  <div class="tabs-item" @click="onClick" :class="classes" :data-name="name">
     <slot></slot>
   </div>
 </template>
@@ -31,23 +31,26 @@ export default {
     },
   },
   created() {
-    this.eventBus.$on("update:selected", (name) => {
-      this.active = name === this.name;
-    });
+    if (this.eventBus) {
+      this.eventBus.$on("update:selected", (name) => {
+        this.active = name === this.name;
+      });
+    }
   },
   methods: {
     onClick() {
       if (this.disabled) {
         return;
       }
-      this.eventBus.$emit("update:selected", this.name, this);
+      this.eventBus && this.eventBus.$emit("update:selected", this.name, this);
+      this.$emit('click',this)
     },
   },
 };
 </script>
 <style lang="scss" scoped>
 $blue: blue;
-$disabled-text-color:grey;
+$disabled-text-color: grey;
 .tabs-item {
   flex-shrink: 0;
   padding: 0 1em;
@@ -59,8 +62,9 @@ $disabled-text-color:grey;
     color: $blue;
     font-weight: bold;
   }
-  &.disabled{
-    color:$disabled-text-color
+  &.disabled {
+    color: $disabled-text-color;
+    cursor: not-allowed;
   }
 }
 </style>
