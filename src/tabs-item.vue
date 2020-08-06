27aa11012fbf4e1a50205ca7fd55c16e87124376
lixiaohu123147 <7595644+lixiaohu123147@user.noreply.gitee.com>
2020-08-06 13:08:10 +0800

    触发 update:selected 事件的时候添加一个 item 数据

--- src/tabs-item.vue
+++ src/tabs-item.vue
@@ -4,54 +4,55 @@
   </div>
 </template>
 <script>
-  export default {
-    name: 'GuluTabsItem',
-    inject: ['eventBus'],
-    data () {
+export default {
+  name: "GuluTabsItem",
+  inject: ["eventBus"],
+  data() {
+    return {
+      active: false,
+    };
+  },
+  props: {
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+    name: {
+      type: String | Number,
+      required: true,
+    },
+  },
+  computed: {
+    classes() {
       return {
-        active: false
-      }
+        active: this.active,
+      };
     },
-    props: {
-      disabled: {
-        type: Boolean,
-        default: false
-      },
-      name: {
-        type: String | Number,
-        required: true
-      }
+  },
+  created() {
+    this.eventBus.$on("update:selected", (name) => {
+      this.active = name === this.name;
+    });
+  },
+  methods: {
+    xxx() {
+      this.eventBus.$emit("update:selected", this.name, this);
     },
-    computed: {
-      classes () {
-        return {
-          active: this.active
-        }
-      }
-    },
-    created () {
-      this.eventBus.$on('update:selected', (name) => {
-        this.active = name === this.name;
-      })
-    },
-    methods: {
-      xxx () {
-        this.eventBus.$emit('update:selected', this.name)
-      }
-    }
-  }
+  },
+};
 </script>
 <style lang="scss" scoped>
-  .tabs-item {
-    flex-shrink: 0;
-    padding: 0 1em;
-    cursor: pointer;
-    border: 1px solid green;
-    height: 100%;
-    display: flex;
-    align-items: center;
-    &.active {
-      background: red;
-    }
+$blue: blue;
+.tabs-item {
+  flex-shrink: 0;
+  padding: 0 1em;
+  cursor: pointer;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  &.active {
+    color: $blue;
+    font-weight: bold;
   }
+}
 </style>
