a974517e1aa88627b5aaa9c072dbc34a01ed9a19
lixiaohu123147 <7595644+lixiaohu123147@user.noreply.gitee.com>
2020-08-06 15:56:22 +0800

    支持 disabled 功能

--- src/tabs-item.vue
+++ src/tabs-item.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="tabs-item" @click="xxx" :class="classes">
+  <div class="tabs-item" @click="onClick" :class="classes">
     <slot></slot>
   </div>
 </template>
@@ -26,6 +26,7 @@ export default {
     classes() {
       return {
         active: this.active,
+        disabled: this.disabled,
       };
     },
   },
@@ -35,7 +36,10 @@ export default {
     });
   },
   methods: {
-    xxx() {
+    onClick() {
+      if (this.disabled) {
+        return;
+      }
       this.eventBus.$emit("update:selected", this.name, this);
     },
   },
@@ -43,6 +47,7 @@ export default {
 </script>
 <style lang="scss" scoped>
 $blue: blue;
+$disabled-text-color:grey;
 .tabs-item {
   flex-shrink: 0;
   padding: 0 1em;
@@ -53,6 +58,9 @@ $blue: blue;
   &.active {
     color: $blue;
     font-weight: bold;
+  }
+  &.disabled{
+    color:$disabled-text-color
   }
 }
 </style>
