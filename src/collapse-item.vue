965bfda5ac6aa81a793ed83c71b0253819827c9b
lixiaohu123147 <7595644+lixiaohu123147@user.noreply.gitee.com>
2020-08-11 16:04:24 +0800

    设置默认 selected

--- src/collapse-item.vue
+++ src/collapse-item.vue
@@ -9,8 +9,13 @@
 
 <script>
 export default {
+  name:"GuluCollapseItem",
   props: {
     title: {
+      type: String,
+      required: true,
+    },
+    name: {
       type: String,
       required: true,
     },
@@ -23,9 +28,11 @@ export default {
   inject: ["eventBus"],
   mounted() {
     this.eventBus &&
-      this.eventBus.$on("update:selected", (vm) => {
-        if (vm !== this) {
+      this.eventBus.$on("update:selected", (name) => {
+        if (name !== this.name) {
           this.close();
+        }else{
+          this.show()
         }
       });
   },
@@ -35,44 +42,32 @@ export default {
         this.open = false;
       } else {
         this.open = true;
-        this.eventBus && this.eventBus.$emit("update:selected", this);
+        this.eventBus && this.eventBus.$emit("update:selected", this.name);
       }
     },
     close() {
       this.open = false;
     },
+    show(){
+      this.open = true
+    }
   },
 };
 </script>
 
-<style lang="scss" scoped>
-$grey: #ddd;
-$border-radius: 4px;
-.collapseItem {
-  > .title {
-    border: 1px solid $grey;
-    margin-top: -1px;
-    margin-left: -1px;
-    margin-right: -1px;
-    min-height: 32px;
-    display: flex;
-    align-items: center;
-    padding: 0 8px;
-  }
-  &:first-child {
-    > .title {
-      border-top-left-radius: $border-radius;
-      border-top-right-radius: $border-radius;
+<style scoped lang="scss">
+  $grey: #ddd;
+  $border-radius: 4px;
+  .collapseItem {
+    > .title { border: 1px solid $grey; margin-top: -1px; margin-left: -1px; margin-right: -1px;
+      min-height: 32px; display: flex; align-items: center; padding: 0 8px;
     }
-  }
-  &:last-child {
-    > .title:last-child {
-      border-bottom-left-radius: $border-radius;
-      border-bottom-right-radius: $border-radius;
+    &:first-child {
+      > .title { border-top-left-radius: $border-radius; border-top-right-radius: $border-radius; }
     }
+    &:last-child {
+      > .title:last-child { border-bottom-left-radius: $border-radius; border-bottom-right-radius: $border-radius; }
+    }
+    > .content { padding: 8px; }
   }
-  > .content {
-    padding: 8px;
-  }
-}
 </style>
--- src/collapse.vue
+++ src/collapse.vue
@@ -5,26 +5,42 @@
 </template>
 
 <script>
-import Vue from "vue";
-export default {
-  name: "GuluCollapse",
-  props: {
-    single: {
-      type: Boolean,
-      default: false,
+  import Vue from 'vue'
+  export default {
+    name: "GuluCollapse",
+    props: {
+      single: {
+        type: Boolean,
+        default: false
+      },
+      selected: {
+        type: String,
+      }
     },
-  },
-  data() {
-    return {
-      eventBus: new Vue(),
-    };
-  },
-  provide() {
-    if (this.single) {
+    data () {
       return {
-        eventBus: this.eventBus,
-      };
+        eventBus: new Vue()
+      }
+    },
+    provide () {
+      return {
+        eventBus: this.eventBus
+      }
+    },
+    mounted () {
+      this.eventBus.$emit('update:selected', this.selected)
+      this.eventBus.$on('update:selected', (name) => {
+        this.$emit('update:selected', name)
+      })
     }
-  },
-};
+  }
 </script>
+
+<style scoped lang="scss">
+  $grey: #ddd;
+  $border-radius: 4px;
+  .collapse {
+    border: 1px solid $grey;
+    border-radius: $border-radius;
+  }
+</style>
