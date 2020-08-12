d42ddc47183e5a0c362d8722c7173aff758d6314
lixiaohu123147 <7595644+lixiaohu123147@user.noreply.gitee.com>
2020-08-12 09:42:35 +0800

    selected 改为数组

--- src/collapse-item.vue
+++ src/collapse-item.vue
@@ -9,7 +9,7 @@
 
 <script>
 export default {
-  name:"GuluCollapseItem",
+  name: "GuluCollapseItem",
   props: {
     title: {
       type: String,
@@ -28,46 +28,55 @@ export default {
   inject: ["eventBus"],
   mounted() {
     this.eventBus &&
-      this.eventBus.$on("update:selected", (name) => {
-        if (name !== this.name) {
-          this.close();
-        }else{
-          this.show()
+      this.eventBus.$on("update:selected", (names) => {
+        if (names.indexOf(this.name) >= 0) {
+          this.open = true;
+        } else {
+          this.open = false;
         }
       });
   },
   methods: {
     toggle() {
       if (this.open) {
-        this.open = false;
+        this.eventBus && this.eventBus.$emit('update:removeSelected',this.name)
       } else {
         this.open = true;
-        this.eventBus && this.eventBus.$emit("update:selected", this.name);
+        this.eventBus && this.eventBus.$emit("update:addSelected", this.name);
       }
     },
-    close() {
-      this.open = false;
-    },
-    show(){
-      this.open = true
-    }
   },
 };
 </script>
 
 <style scoped lang="scss">
-  $grey: #ddd;
-  $border-radius: 4px;
-  .collapseItem {
-    > .title { border: 1px solid $grey; margin-top: -1px; margin-left: -1px; margin-right: -1px;
-      min-height: 32px; display: flex; align-items: center; padding: 0 8px;
-    }
-    &:first-child {
-      > .title { border-top-left-radius: $border-radius; border-top-right-radius: $border-radius; }
-    }
-    &:last-child {
-      > .title:last-child { border-bottom-left-radius: $border-radius; border-bottom-right-radius: $border-radius; }
-    }
-    > .content { padding: 8px; }
+$grey: #ddd;
+$border-radius: 4px;
+.collapseItem {
+  > .title {
+    border: 1px solid $grey;
+    margin-top: -1px;
+    margin-left: -1px;
+    margin-right: -1px;
+    min-height: 32px;
+    display: flex;
+    align-items: center;
+    padding: 0 8px;
   }
+  &:first-child {
+    > .title {
+      border-top-left-radius: $border-radius;
+      border-top-right-radius: $border-radius;
+    }
+  }
+  &:last-child {
+    > .title:last-child {
+      border-bottom-left-radius: $border-radius;
+      border-bottom-right-radius: $border-radius;
+    }
+  }
+  > .content {
+    padding: 8px;
+  }
+}
 </style>
