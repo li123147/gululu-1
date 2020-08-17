b18fe1d0353ffd06b5eec521a3a91f4a5e5e6f51
lixiaohu123147 <7595644+lixiaohu123147@user.noreply.gitee.com>
2020-08-17 11:45:17 +0800

    建立组件md

--- docs/.vuepress/components/button-demos.vue
+++ docs/.vuepress/components/button-demos.vue
@@ -1,3 +1,4 @@
+
 <template>
   <div>
     <g-button>默认按钮</g-button>
@@ -5,9 +6,7 @@
     <g-button :loading="true">默认按钮</g-button>
     <g-button disabled>默认按钮</g-button>
 
-    <pre>
-    <code>{{content}}</code>
-    </pre>
+    <pre><code>{{content}}</code></pre>
   </div>
 </template>
 <script>
@@ -19,12 +18,12 @@
     data () {
       return {
         content: `
-        <g-button>默认按钮</g-button>
-        <g-button icon="settings">默认按钮</g-button>
-        <g-button :loading="true">默认按钮</g-button>
-        <g-button disabled>默认按钮</g-button>
-      `
+          <g-button>默认按钮</g-button>
+          <g-button icon="settings">默认按钮</g-button>
+          <g-button :loading="true">默认按钮</g-button>
+          <g-button disabled>默认按钮</g-button>
+      `.replace(/\t+| +/g, '').trim()
       }
     }
   }
-</script> 
+</script>
